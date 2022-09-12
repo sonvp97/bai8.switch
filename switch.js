2636c488627e370dc9292e4395b7dc85fbf0af7b
sonvp97 <112803870+sonvp97@users.noreply.github.com>
2022-09-12 11:01:28 +0700

    first commit

---
+++ switch.js
@@ -0,0 +1,13 @@
+let brower = prompt("Enter browser name!");
+switch (brower){
+    case 'Edge':
+        alert("You've got the Edge!");
+        break;
+    case 'Chrome':
+    case 'Firefox':
+    case 'Safari':
+    case 'Opera':
+        alert('Okay we support these browsers too');
+    default:
+        alert('We hope that this page looks ok!');
+}
