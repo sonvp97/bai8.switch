b9053076b40010b2790325706a08eb70412f96a6
sonvp97 <112803870+sonvp97@users.noreply.github.com>
2022-09-12 15:51:49 +0700

    aa

--- bai3.js
+++ bai3.js
@@ -1,11 +1,13 @@
 let value=parseInt(prompt('Nhập số'));
-switch (value){
-    case 0:
-        alert(0);
-        break;
-    case !0:
+tuyetdoi=Math.abs(value);
+number=value/tuyetdoi;
+switch (number){
+    case 1:
         alert(1);
         break;
+    case -1:
+        alert(-1);
+        break;
     default:
-        alert(-1)
+        alert(0);
 }
